b8a3795517f8e8daf009079926219715b09fa93c
Vlad <vladsilagy@gmail.com>
2022-11-16 01:26:23 -0500

    finish navbar layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,46 +1,51 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../public/Tesla-logo.svg';
 const Navbar = () => {
 	return (
 		<nav className="absolute w-full px-8 bg-transparent flex justify-between items-center font-medium text-sm">
 			<div className="p-5">
-				<Image
-					src={logo}
-					alt="tesla logo"
-					className="w-[120px] h-6"
-				/>
+				<Link href="/">
+					<Image
+						src={logo}
+						alt="tesla logo"
+						className="w-[120px] h-6 lg:mr-16"
+					/>
+				</Link>
 			</div>
 			<div className="hidden lg:block">
-				<ul className="flex gap-8">
-					<li>
+				<ul className="flex">
+					<li className="py-2 px-4 hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/models'>Model S</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/model3'>Model 3</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/modelx'>Model X</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/modely'>Model Y</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/solarroof'>Solar Roof</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/solarpanels'>Solar Panels</a>
 					</li>
 				</ul>
 			</div>
 			<div className="hidden lg:block">
-				<ul className="flex gap-8">
-					<li>
+				<ul className="flex">
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href="https://shop.tesla.com">Shop</a>
 					</li>
-					<li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href="https://tesla.com/teslaaccount">Account</a>
 					</li>
-					<li>Menu</li>
+					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+						Menu
+					</li>
 				</ul>
 			</div>
 		</nav>
