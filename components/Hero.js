ddb99167fe46e381cf831a030cb97c30b7a15071
Vlad <vladsilagy@gmail.com>
2022-11-15 23:57:09 -0500

    add assets, images and fonts

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+	return (
+		<section className="h-screen w-screen">
+			<div className="flex h-screen items-center justify-center bg-no-repeat bg-cover bg-center bg-[url('/Model3-Mobile.jpeg')] lg:bg-[url('/Model3-Desktop.jpeg')]">
+				<div className="-mt-96">
+					<h1 className="text-4xl">Model 3</h1>
+					<p>Order Onlife for Touchless Delivery</p>
+				</div>
+			</div>
+		</section>
+	);
+}
