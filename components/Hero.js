d24d3601ab013e898fbdf9211b30abf5e4c72fca
Vlad <vladsilagy@gmail.com>
2022-11-28 23:35:43 -0500

    center header, fixed navbar transition

--- components/Hero.js
+++ components/Hero.js
@@ -2,10 +2,14 @@ const Hero = () => {
 	return (
 		<section className="h-screen w-screen">
 			<div className="flex h-screen items-center justify-center bg-no-repeat bg-cover bg-center bg-[url('/Model3-Mobile.jpeg')] lg:bg-[url('/Model3-Desktop.jpeg')]">
-				<div className="-mt-96 text-center space-y-3">
+				<div className="-mt-[36rem] text-center space-y-3">
 					<h1 className="text-[40px] font-medium">Model 3</h1>
 					<p className="text-sm font-normal">
-						Order Onlife for Touchless Delivery
+						<a
+							href="https://www.tesla.com/drive"
+							className="underline">
+							Schedule a Test Drive
+						</a>
 					</p>
 				</div>
 			</div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import logo from '../public/Tesla-logo.svg';
 const Navbar = () => {
 	return (
-		<nav className="absolute w-full px-8 bg-transparent flex justify-between items-center font-medium text-sm">
+		<nav className="absolute w-full px-4 bg-transparent flex justify-between items-center font-medium text-sm">
 			<div className="p-5">
 				<Link href="/">
 					<Image
@@ -13,7 +13,7 @@ const Navbar = () => {
 					/>
 				</Link>
 			</div>
-			<div className="hidden lg:block">
+			<div className="hidden xl:block">
 				<ul className="flex">
 					<li className="py-2 px-4 hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href='"https://tesla.com/models'>Model S</a>
@@ -35,7 +35,7 @@ const Navbar = () => {
 					</li>
 				</ul>
 			</div>
-			<div className="hidden lg:block">
+			<div className="hidden xl:block">
 				<ul className="flex">
 					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
 						<a href="https://shop.tesla.com">Shop</a>
@@ -47,6 +47,11 @@ const Navbar = () => {
 						Menu
 					</li>
 				</ul>
+			</div>
+			<div className="xl:hidden">
+				<button className="py-2 px-4 bg-black bg-opacity-5 rounded">
+					Menu
+				</button>
 			</div>
 		</nav>
 	);
