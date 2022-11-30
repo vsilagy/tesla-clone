b35e99f0de65204d434a5587cc6fdb746cffacb0
Vlad <vladsilagy@gmail.com>
2022-11-29 20:11:35 -0500

    add side navbar

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,9 @@
-import { BsChevronDown } from 'react-icons/bs';
+import { HiChevronDown } from 'react-icons/hi2';
 const Hero = () => {
 	return (
 		<section className="h-screen w-screen">
-			<div className="flex flex-col h-screen items-center justify-between bg-no-repeat bg-cover bg-center bg-[url('/Model3-Mobile.jpeg')] lg:bg-[url('/Model3-Desktop.jpeg')]">
-				<div className="mt-36 text-center ">
+			<div className="relative flex flex-col h-screen items-center bg-no-repeat bg-cover bg-center bg-[url('/Model3-Mobile.jpeg')] lg:bg-[url('/Model3-Desktop.jpeg')]">
+				<div className="absolute inset-x-0 top-[15%] text-center ">
 					<h1 className="text-[40px] font-medium">Model 3</h1>
 					<p className="text-sm font-normal">
 						<a
@@ -13,17 +13,21 @@ const Hero = () => {
 						</a>
 					</p>
 				</div>
-				<div className="flex flex-col items-center gap-16 mb-8">
-					<div className="space-x-8 text-white">
-						<button className="py-2 px-16 bg-gray-700 rounded">
+				<div className="absolute inset-x-0 bottom-6 w-full flex flex-col items-center gap-14">
+					<div className=" flex flex-col md:flex-row justify-center gap-4 sm:gap-6 w-full px-5 text-center text-sm font-medium">
+						<a
+							href="https://www.tesla.com/model3/design"
+							className="py-3 flex-1 sm:flex-none md:w-72 bg-[#171A20]/80 text-white rounded">
 							Custom Order
-						</button>
-						<button className="py-2 px-16 bg-gray-300 rounded">
+						</a>
+						<a
+							href="https://www.tesla.com/inventory/new/m3"
+							className="py-3 flex-1 sm:flex-none md:w-72 bg-[#F4F4F4]/80 text-gray-700 rounded">
 							Existing Inventory
-						</button>
+						</a>
 					</div>
 					<button>
-						<BsChevronDown />
+						<HiChevronDown className="text-gray-700" size={24} />
 					</button>
 				</div>
 			</div>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,59 +1,151 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/Tesla-logo.svg';
+import { GrClose } from 'react-icons/gr';
+import { HiChevronRight } from 'react-icons/hi';
 const Navbar = () => {
 	return (
-		<nav className="absolute w-full px-4 bg-transparent flex justify-between items-center font-medium text-sm">
-			<div className="p-5">
-				<Link href="/">
-					<Image
-						src={logo}
-						alt="tesla logo"
-						className="w-[120px] h-6 lg:mr-16"
-					/>
-				</Link>
-			</div>
-			<div className="hidden xl:block">
-				<ul className="flex">
-					<li className="py-2 px-4 hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/models'>Model S</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/model3'>Model 3</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/modelx'>Model X</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/modely'>Model Y</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/solarroof'>Solar Roof</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href='"https://tesla.com/solarpanels'>Solar Panels</a>
-					</li>
-				</ul>
-			</div>
-			<div className="hidden xl:block">
-				<ul className="flex">
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href="https://shop.tesla.com">Shop</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
-						<a href="https://tesla.com/teslaaccount">Account</a>
-					</li>
-					<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+		<>
+			<Head>
+				<title>
+					Tesla Clone | Electric Cars, Solar & Clean Energy
+				</title>
+				<meta
+					name="description"
+					content="Tesla is accelerating the world's transition to sustainable energy with electric cars, solar and integrated renewable energy solutions for homes and businesses."
+				/>
+			</Head>
+			<nav className="absolute z-10 w-full px-4 bg-transparent flex justify-between items-center font-medium text-sm">
+				<div className="p-5">
+					<Link href="/">
+						<Image
+							src={logo}
+							alt="tesla logo"
+							className="w-[120px] h-6 lg:mr-16"
+						/>
+					</Link>
+				</div>
+				<div className="hidden xl:block">
+					<ul className="flex">
+						<li className="py-2 px-4 hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/models'>Model S</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/model3'>Model 3</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/modelx'>Model X</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/modely'>Model Y</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/solarroof'>Solar Roof</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href='"https://tesla.com/solarpanels'>
+								Solar Panels
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="hidden xl:block">
+					<ul className="flex">
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href="https://shop.tesla.com">Shop</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							<a href="https://tesla.com/teslaaccount">Account</a>
+						</li>
+						<li className="py-2 px-4  hover:bg-black hover:bg-opacity-5 hover:rounded">
+							Menu
+						</li>
+					</ul>
+				</div>
+				<div className="xl:hidden">
+					<button className="py-2 px-4 bg-black bg-opacity-5 rounded">
 						Menu
+					</button>
+				</div>
+			</nav>
+			<div className="absolute top-0 right-0 w-80 h-full z-10 bg-white">
+				<div className="flex justify-end pt-5 px-8">
+					<GrClose
+						className="p-2 rounded hover:bg-gray-200"
+						size={32}
+					/>
+				</div>
+				<ul className="flex flex-col mt-10 mx-8 gap-3  text-sm text-[#393C41] font-medium">
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Model S
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Model 3
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Model X
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Model Y
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Solar Roof
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Solar Panel
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Existing Inventory
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Used Inventory
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Trade-In
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Test Drive
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Insurance
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Powerwall
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Comercial Energy
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Utilities
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Charging
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Find Us
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Support
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Investor Relations
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Shop
+					</li>
+					<li className="px-3 py-1 mr-4 rounded hover:bg-black/5  cursor-pointer">
+						Account
+					</li>
+					<li className="flex items-center justify-between mr-4 px-3 py-1 rounded hover:bg-black/5  cursor-pointer">
+						More
+						<span>
+							<HiChevronRight size={18} />
+						</span>
 					</li>
 				</ul>
 			</div>
-			<div className="xl:hidden">
-				<button className="py-2 px-4 bg-black bg-opacity-5 rounded">
-					Menu
-				</button>
-			</div>
-		</nav>
+		</>
 	);
 };
 
